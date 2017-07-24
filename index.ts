d448b1dd0a68d50001b1f442e2b40bf85210c5b8
Sam Saccone <samccone@gmail.com>
2017-07-23 21:29:22 -0700

    Add a simple index file to generate the interface

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
-// handle escaped strings
-// when flattening do not lose types
-// handle nested dom-repeat printing
+import * as fs from "fs";
+import * as Cheerio from "cheerio";
+import { walkNodes } from "./dom_walker";
+import { AliasMap, AST_NODE } from "./types";
+import { getExpressionsForNode } from "./ast_builder";
+import { nodesToTree } from "./ast_tree_builder";
+import { printTree } from "./printer";
+
+export function generateInterface(htmlPath: string) {
+  const nodes: AST_NODE[] = [];
+  const sample = fs.readFileSync(htmlPath, "utf-8");
+  const parsed = Cheerio.parseHTML(sample);
+  const aliasMap: AliasMap = {};
+
+  parsed.forEach(node => {
+    walkNodes(node, aliasMap, (node: CheerioElement, aliasMap: AliasMap) => {
+      nodes.push(...getExpressionsForNode(node, aliasMap));
+    });
+  });
+
+  console.log(printTree(nodesToTree(nodes)));
+}
